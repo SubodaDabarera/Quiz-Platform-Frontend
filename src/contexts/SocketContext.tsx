d291b31b0d201d0f0824e4a6a578a539bd12db07
SubodaDabarera <SubodaDabarera99@gmail.com>
2025-03-01 17:08:56 +0100

    socket connection added

---
+++ src/contexts/SocketContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+
+const SocketContext = createContext<Socket | null>(null);
+
+const VITE_API_URL = "http://localhost:5000"
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = io(VITE_API_URL);
+    setSocket(newSocket);
+    return () => { newSocket.disconnect(); };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocket = () => useContext(SocketContext);
